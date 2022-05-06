c3b857706faa1bc37885f6bb5fd984cc66ed67fb
Jacob <jbg272@cornell.edu>
2022-05-06 15:53:03 -0400

    Final Jacob Push

--- Jacob Analysis.R
+++ Jacob Analysis.R
@@ -4,6 +4,7 @@ library(boot)
 library(leaps)
 library(Matrix)
 library(pls)
+library(fastDummies)
 
 # Cleaning data and removing outliers
 
@@ -11,7 +12,15 @@ df <- read.csv("WineData.csv")
 
 df <- na.omit(df)
 df <- subset(df, select = -c(X, Id, Name, Winery, StyleName, Region))
-df <- subset(df, Acidity != -1 & Year > 1950 & Country != "Mexico")
+df <- subset(df, Acidity != -1 & Year > 1950 & Country != "Mexico" & Country != "NONE")
+
+df <- dummy_cols(df, select_columns = 'Type')
+df <- dummy_cols(df, select_columns = 'Vintage')
+df <- dummy_cols(df, select_columns = 'Nat')
+df <- dummy_cols(df, select_columns = 'Country')
+df <- dummy_cols(df, select_columns = 'Size')
+
+#df <- subset(df, select = c(Type, Vintage, Nat, Country, Size))
 
 df$Type <- as.factor(df$Type)
 df$Type <- model.matrix( ~ Type - 1, data=df)
@@ -40,37 +49,13 @@ dfRating.post <- df[abs(dfRatingRes) < 3,]
 dfPrice.post <- na.omit(dfPrice.post)
 dfRating.post <- na.omit(dfRating.post)
 
-# PCA to pick Largest Components
-
-Price_pr.out <- prcomp(dfPrice.post[-c(1)], scale=TRUE)
-
-Price_pcr.fit <- pcr(Price ~ ., data = dfPrice.post, scale=TRUE, validation = "CV")
-
-test <- predict(Price_pr.out, dfPrice.post[-c(1)])
-
-dfPrice.post[ , which(apply(dfPrice.post, 2, var) == 0)]
-
-
-which(apply(dfRating.post, 2, var)==0)
-dfRating.post[ , which(apply(dfRating.post, 2, var) != 0)]
-
-Rating_pr.out <- prcomp(subset(dfRating.post, select = -c(Rating)) , scale=TRUE)
-
-#
-
-rankifremoved <- sapply(1:ncol(dfPrice.post), function (x) qr(dfPrice.post[,-x])$rank)
-which(rankifremoved == max(rankifremoved))
-
-rankifremoved <- sapply(1:ncol(dfRating.post), function (x) qr(dfRating.post[,-x])$rank)
-which(rankifremoved == max(rankifremoved))
-
 # Training Linear Models and Checking for Non-constant Error Variance
 
 set.seed(1)
 trainPrice <- sample(1:nrow(dfPrice.post), nrow(dfPrice.post) * 0.6)
 trainRating <- sample(1:nrow(dfRating.post), nrow(dfRating.post) * 0.6)
 
-Price_lm.fit <- lm(Price ~ ., data = dfPrice.post, subset = trainPrice)
+Price_lm.fit <- glm(Price ~ ., data = dfPrice.post)
 Rating_lm.fit <- lm(Rating ~ ., data = dfRating.post, subset = trainRating)
 
 dfPrice.post_test <- dfPrice.post[-trainPrice,]
@@ -97,18 +82,14 @@ Rating.predict_transform <- predict.lm(Rating_lm.transform_fit, dfRating.post_te
 Price_resid.transform <- dfPrice.post_test$Price - Price.predict_transform
 Rating_resid.transform <- dfRating.post_test$Rating - Rating.predict_transform
 
-plot(dfPrice.post_test$Price, Price_resid.transform, xlab = "Fitted values", ylab = "Residuals", main = "Price Response")
-plot(dfRating.post_test$Rating, Rating_resid.transform, xlab = "Fitted values", ylab = "Residuals", main = "Rating Response")
+plot(dfPrice.post_test$Price, Price_resid.transform, xlab = "Fitted values", ylab = "Residuals", main = "log(Price) Response")
+plot(dfRating.post_test$Rating, Rating_resid.transform, xlab = "Fitted values", ylab = "Residuals", main = "log(Rating) Response")
 
-# 5 vs 10 Fold Cross Validation (Pointless)
+mean((Price_resid.transform)^2)
+mean((Rating_resid.transform)^2)
 
-Price_glm.fit <- glm(Price ~ ., data = dfPrice.post)
-Rating_glm.fit <- glm(Rating ~ ., data = dfRating.post)
-
-Price_cv.error5 = cv.glm(dfPrice.post, Price_glm.fit, K=5)$delta[1]
-Rating_cv.error5 = cv.glm(dfRating.post, Rating_glm.fit, K=5)$delta[1]
-Price_cv.error10 = cv.glm(dfPrice.post, Price_glm.fit, K=5)$delta[1]
-Rating_cv.error10 = cv.glm(dfRating.post, Rating_glm.fit, K=10)$delta[1]
+summary(Price_lm.transform_fit)
+summary(Rating_lm.transform_fit)
 
 # Best Subset Selection
 
@@ -118,12 +99,28 @@ dfPrice.post <- as.data.frame(scale(dfPrice.post[2:11]))
 
 dfPrice.post <- cbind(Price, dfPrice.post)
 
-Price_regfit.full <- regsubsets(Price ~ ., data = dfPrice.post, nvmax = 30)
-Rating_regfit.full <- regsubsets(Rating ~ ., data = dfRating.post, nvmax = 30)
+Price_regfit.full <- regsubsets(log(Price) ~ ., data = dfPrice.post[trainPrice,], nvmax = 30)
+Rating_regfit.full <- regsubsets(log(Rating) ~ ., data = dfRating.post[trainRating,], nvmax = 30)
 
-plot(summary(Price_regfit.full)$adjr2, type="b", xlab="p", ylab=expression(R^2))
-plot(summary(Rating_regfit.full)$adjr2, type = "b", xlab = "p", ylab = expression(R^2))
-plot(summary(Price_regfit.full)$bic, type="b", xlab="p", ylab="BIC")
-plot(summary(Rating_regfit.full)$bic, type="b", xlab="p", ylab="BIC")
-plot(summary(Price_regfit.full)$cp, type="b", xlab="p", ylab=expression(C[p]))
-plot(summary(Rating_regfit.full)$cp, type="b", xlab="p", ylab=expression(C[p]))
+plot(summary(Price_regfit.full)$adjr2, type="b", xlab="# of Predictors", ylab=expression(R^2), main = "Best Subset Selection of Price Regression - R^2")
+plot(summary(Rating_regfit.full)$adjr2, type = "b", xlab = "p", ylab = expression(R^2), main = "Best Subset Selection of Rating Regression - R^2")
+plot(summary(Price_regfit.full)$cp, type="b", xlab="p", ylab=expression(C[p]), main = "Best Subset Selection of Price Regression - Cp")
+plot(summary(Rating_regfit.full)$cp, type="b", xlab="p", ylab=expression(C[p]), main = "Best Subset Selection of Rating Regression - Cp")
+
+which.max(summary(Price_regfit.full)$adjr2)
+which.min(summary(Price_regfit.full)$cp)
+which.max(summary(Rating_regfit.full)$adjr2)
+which.min(summary(Rating_regfit.full)$cp)
+
+summary(Price_regfit.full)$adjr2[24]
+summary(Price_regfit.full)$cp[20]
+
+summary(Rating_regfit.full)$adjr2[20]
+
+coef(Price_regfit.full, 20)
+coef(Rating_regfit.full, 23)
+
+test_mat = model.matrix (Rating~., data = dfRating.post[-trainRating,])
+coefi = coef(Rating_regfit.full, id = 20)
+pred = test_mat[,names(coefi)]%*%coefi
+val_errors = mean((dfRating.post$Rating[-trainRating]-pred)^2)
